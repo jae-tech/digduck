8b6d9204b17dd3b3abbc454a4a66fed71fa50ccb
JaeTech <cinnamatcha@gmail.com>
2025-08-11 20:57:53 +0900

    feat(smart-store): 제품 크롤링 및 리뷰 크롤링 기능 추가

--- apps/api/src/controllers/smart-store.controller.ts
+++ apps/api/src/controllers/smart-store.controller.ts
@@ -1,23 +1,72 @@
-import { Controller, Get, Schema } from "@/decorators/controller.decorator";
-import { crawlProduct } from "@/services/crawl.service";
+import {
+  Controller,
+  Get,
+  Post,
+  Schema,
+} from "@/decorators/controller.decorator";
+import { crawlService, comparisonService } from "@/services";
+import { FastifyRequest } from "fastify";
 
 @Controller("/smart-store")
 export class SmartStoreController {
-  @Get("/crawl")
+  @Get("/crawl/product/:keyword")
   @Schema({
     description: "Crawl product data from Smart Store",
-    tags: ["users"],
+    tags: ["products"],
+    security: [{ bearerAuth: [] }],
+    required: ["keyword"],
+    params: {
+      type: "object",
+      properties: {
+        keyword: { type: "string", minLength: 2, maxLength: 50 },
+      },
+    },
+  })
+  async crawlProduct(request: FastifyRequest<{ Params: { keyword: string } }>) {
+    const data = await crawlService.crawlProduct(request.params.keyword);
+    return data;
+  }
+
+  @Post("/crawl/reviews")
+  @Schema({
+    description: "Crawl product reviews from Smart Store",
+    tags: ["products"],
+    security: [{ bearerAuth: [] }],
+    required: ["url"],
+    params: {
+      type: "object",
+      properties: {
+        url: { type: "string", format: "uri" },
+      },
+    },
+  })
+  async crawlReviews(request: FastifyRequest<{ Params: { url: string } }>) {
+    // const { url } = request.params;
+
+    const url = "https://smartstore.naver.com/marketnuttre/products/9649413233";
+
+    const data = await crawlService.crawlReviews(url);
+    return data;
+  }
+
+  @Get("/comparison/:keyword")
+  @Schema({
+    description: "Get competitive pricing for a product",
+    tags: ["products"],
     security: [{ bearerAuth: [] }],
     params: {
       type: "object",
       properties: {
-        id: { type: "string", format: "uuid" },
+        keyword: { type: "string", minLength: 2, maxLength: 50 },
       },
-      required: ["id"],
+      required: ["keyword"],
     },
   })
-  async crawlProduct(url: string) {
-    const data = await crawlProduct(url);
+  async getCompetitivePricing(
+    request: FastifyRequest<{ Params: { keyword: string } }>
+  ) {
+    const { keyword } = request.params;
+    const data = await comparisonService.findOptimalPrice(keyword);
     return data;
   }
 }
