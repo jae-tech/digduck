65fd51e2cf96a24e2ca47caa1264eedec0e91e06
JaeTech <cinnamatcha@gmail.com>
2025-08-11 20:58:54 +0900

    feat(types): API 타입 정의 파일 생성 및 기존 타입들 내보내기 추가

---
+++ apps/api/src/types/index.ts
@@ -0,0 +1,14 @@
+// API 타입들
+export * from "./api/naver-shopping.types";
+// export * from "./api/external-api.types";
+
+// DTO 타입들
+// export * from "./dto/comparison.dto";
+// export * from "./dto/product.dto";
+
+// 공통 타입들
+// export * from "./common/response.types";
+// export * from "./common/pagination.types";
+
+// 기존 타입들
+export * from "./auth.types";
