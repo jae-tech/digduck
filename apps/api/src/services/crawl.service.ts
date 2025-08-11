d1c1d5b73b6844b447939ba519fc5a4d6b2d154d
허성재 <heo0201@hectonproject.com>
2025-08-11 20:58:34 +0900

    feat(crawl-service): 크롤링 서비스 클래스 및 제품/리뷰 크롤링 기능 추가

--- apps/api/src/services/crawl.service.ts
+++ apps/api/src/services/crawl.service.ts
@@ -1,7 +1,14 @@
-import { crawlSmartstore } from "@/cralwer/smart-store.crawler";
+import { crawlLowPrice, crawlReviews } from "@/cralwer/smart-store.crawler";
 
-export async function crawlProduct(url: string) {
-  // 여기에 DB 저장 로직이나 데이터 변환 로직을 추가 가능
-  const data = await crawlSmartstore(url);
-  return { ...data, crawledAt: new Date() };
+export class CrawlService {
+  async crawlProduct(url: string) {
+    // 여기에 DB 저장 로직이나 데이터 변환 로직을 추가 가능
+    const data = await crawlLowPrice(url);
+    return { ...data, crawledAt: new Date() };
+  }
+
+  async crawlReviews(url: string) {
+    const reviews = await crawlReviews(url);
+    return { reviews, crawledAt: new Date() };
+  }
 }
