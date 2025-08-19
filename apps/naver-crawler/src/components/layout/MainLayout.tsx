57dbb7e3b203cd53fc6e51cc65f1bb6f07527745
Xeongjae1 <cinnamatcha@gmail.com>
2025-08-19 23:25:31 +0900

    feat: MainLayout 컴포넌트의 스타일 수정 - 중앙 정렬 및 최대 너비 설정 추가

--- apps/naver-crawler/src/components/layout/MainLayout.tsx
+++ apps/naver-crawler/src/components/layout/MainLayout.tsx
@@ -4,8 +4,8 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-slate-100">
-      <div className="mx-auto p-4">
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <div className="w-full max-w-4xl mx-auto p-4">
         <main>{children}</main>
       </div>
     </div>
