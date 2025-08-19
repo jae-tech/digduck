1f740ed84dc8b6fae4553ceaa4a6f857a85d61e2
JaeTech <cinnamatcha@gmail.com>
2025-08-19 16:16:53 +0900

    fix: utils.ts 파일에서 코드 스타일 수정 - 세미콜론 추가

--- apps/naver-crawler/src/lib/utils.ts
+++ apps/naver-crawler/src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatMilliSecondsToMinutes(num: number) {
+  const minutes = Math.floor(num / 60000);
+  const seconds = Math.floor((num % 60000) / 1000);
+  return `${minutes}분 ${seconds}초`;
 }
