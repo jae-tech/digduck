e0b0bf36a7dd58f782cbcbff184a9bde8cbb0de3
JaeTech <cinnamatcha@gmail.com>
2025-08-19 16:17:10 +0900

    feat: main.tsx 파일 수정 - 라우터 설정 추가 및 App 컴포넌트 제거

--- apps/naver-crawler/src/main.tsx
+++ apps/naver-crawler/src/main.tsx
@@ -1,16 +1,29 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "./index.css";
-import App from "./App.tsx";
 
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+// Create a client
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
