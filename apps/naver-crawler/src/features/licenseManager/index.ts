722f38378a512f2bb83f734625be3b3d780ce5bf
JaeTech <cinnamatcha@gmail.com>
2025-08-20 22:25:07 +0900

    feat: 라이센스 관리 모듈의 인덱스 파일 추가

---
+++ apps/naver-crawler/src/features/licenseManager/index.ts
@@ -0,0 +1,12 @@
+export { LicenseManagerPage } from "./components/LicenseManagerPage";
+export { LicenseTable } from "./components/LicenseTable";
+export { LicenseFilters } from "./components/LicenseFilters";
+export { useLicenseManager } from "./hooks/useLicenseManager";
+export { LicenseManagerService } from "./services/licenseManager.service";
+export type {
+  LicenseRecord,
+  LicenseFilter,
+  LicenseUpdateData,
+  LicenseStats,
+  BulkAction,
+} from "./types/licenseManager.types";
