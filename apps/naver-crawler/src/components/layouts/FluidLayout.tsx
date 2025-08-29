5ba8d76b6322f6970ca04ee068c07f52055a391d
JaeTech <cinnamatcha@gmail.com>
2025-08-29 10:55:44 +0900

    feat: FluidLayout 컴포넌트의 레이아웃 수정 및 스타일 개선

--- apps/naver-crawler/src/components/layouts/FluidLayout.tsx
+++ apps/naver-crawler/src/components/layouts/FluidLayout.tsx
@@ -7,9 +7,7 @@ export default function FluidLayout({
 }) {
   return (
     <div className="min-h-screen bg-slate-100">
-      <div className="w-full max-w-[1440px] mx-auto p-4">
-        <main>{children}</main>
-      </div>
+      <main className="w-full max-w-7xl mx-auto p-4">{children}</main>
     </div>
   );
 }
